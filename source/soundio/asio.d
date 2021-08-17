c3c05a835be447373c83dba0d7e35b83955d6565
dkorpel <dkorpel@gmail.com>
2021-08-17 23:52:02 +0200

    delete empty asio module

--- source/soundio/asio.d
+++
@@ -1,5 +0,0 @@
-module soundio.asio;
-
-// include bassasio.h
-
-// BASS_ASIO_FORMAT_16BITI just ran into a use-case tonight! Maybe. The caveat is that my use case might be pretty easy compared to what Andrei is asking for. My use case requires modifying the function signature a lot, but that can actually be exploited to make my solution easier. I probably only need to forward storage class. It's still kinda leading to code that feels wrong or isn't obvious, so maybe it will help your discussion.
